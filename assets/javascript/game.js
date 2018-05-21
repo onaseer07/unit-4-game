d68732495d788579205d5a980f6d4a91153c614a
onaseer07 <onaseer07@gmail.com>
2018-05-21 18:55:34 -0500

    Added some pseudo codes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,12 @@
-/*
-   * There will be four crystals displayed as buttons on the page.
-
-   * The player will be shown a random number at the start of the game.
-
-   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
-     * Your game will hide this amount until the player clicks a crystal.
-     * When they do click one, update the player's score counter.
-
-   * The player wins if their total score matches the random number from the beginning of the game.
-
-   * The player loses if their score goes above the random number.
-
-   * The game restarts whenever the player wins or loses.
-
-     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-
-##### Option 1 Game design notes
-
-* The random number shown at the start of the game should be between 19 - 120.
-
-* Each crystal should have a random hidden value between 1 - 12.
-
-    <div id = "title"></div>
-    <div id = "rules"></div>
-    <div id = "randomNum"></div>
-    <div id = "scoreboard"></div>
-    <div id = "crystals"></div>
-    <div id = "playerNum"></div>
-*/
-
- 
-
+//Create and assign a varibale to a function that creates a random number.
  let randomNum = function getRandomNum (min,max) {
    return Math.floor(Math.random() * (max-min +1) ) + min;
  }
+ //Declare and store randomly genereated crystals value in an array. 
  var crystalValue = [randomNum(1,12),randomNum(1,12),randomNum(1,12),randomNum(1,12)];
+ //Create and set the player's score as zero at start.
  var playerScore = 0;
+ //Create a target number and assign a randomly generated value between 19 and 120. 
  var target = Math.floor(Math.random() * (120-19 +1) ) + 19;
  var wins = 0;
  var losses = 0;
